c65bae62583c61afc2c0715f18d5565959e7411b
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-07-10 01:58:12 +0100

    directory restructured

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Id magna do incididunt duis deserunt.
+                Proident consequat dolor enim magna.Amet anim ipsum reprehenderit reprehenderit.
+                Cupidatat ut officia elit laboris ex quis Lorem magna minim aute amet. Culpa ea elit commodo excepteur Lorem ad incididunt dolor. Et laboris elit sit eu enim do in. Reprehenderit tempor eu aliquip id ullamco velit deserunt do minim laboris excepteur.
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ pages/students/index.jsx
@@ -0,0 +1,14 @@
+
+
+const Profiles = () => {
+    return(
+        <div>
+            <h1>List of students</h1>
+            <p>
+                A LIST OF STUDENTS Do commodo mollit exercitation nostrud. Eu cillum cillum id aliqua occaecat veniam cillum id reprehenderit cupidatat sunt proident consectetur do. Ut ad magna aute pariatur pariatur eiusmod cupidatat est id labore ullamco mollit in. Eiusmod in officia dolore quis culpa nisi anim tempor. Sint nostrud adipisicing commodo ad sunt.
+            </p>
+        </div>
+    )
+}
+
+export default Profiles 
