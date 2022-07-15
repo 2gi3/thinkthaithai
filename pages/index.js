6b33d89711b082dba628674174829728f381fef4
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-07-15 05:36:30 +0100

    first draft of the whole homepage completed

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
         <link rel="icon" href="/logo.webp" />
       </Head>
       <div className={styles.background}>
-      <Image src='/homepageBg.jpg' width='400px' height='400px' ></Image>
-            </div>
+        <Image src='/homepageBg.jpg' width='400px' height='400px' ></Image>
+      </div>
       <div className={styles.container}>
         <div className={styles.titleContainer}>
           <div className={styles.logo}>
@@ -29,7 +29,7 @@ export default function Home() {
           <Link href='/#'><button className={styles.button} >Book a free trial lesson</button></Link>
         </div>
         <div className={styles.imageContainer}>
-          <div className={styles.imageBigContainer}>
+          {/* <div className={styles.imageBigContainer}>
             <div className={styles.imageBig}>
               <Image className={styles.image} src='/phone.png' width='100' height='100' />
             </div>
@@ -39,12 +39,12 @@ export default function Home() {
             <div className={styles.imageBig}>
               <Image className={styles.image} src='/tablet.png' width='120' height='100' />
             </div>
-          </div>
+          </div> */}
           {/* <div className={styles.imageSmallContainer}>
             <div className={styles.imageSmall}>
               <Image src='/ipod.png' width='50' height='50' />
             </div>
-            <div className={styles.imageSmall}>
+            <div className={styles.imageSmall}> 
               <Image src='/ear.png' width='50' height='50' />
             </div>
             <div className={styles.imageSmall}>
@@ -88,6 +88,64 @@ export default function Home() {
             <div className={styles.course}>Holiday</div>
             <div className={styles.course}>Business</div>
             <div className={styles.course}>Family</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.peopleContainer}>
+        <div className={styles.teacher}>
+          <div className={styles.teacherVideo}>
+            <h3> Know your teacher and learning style</h3>
+            <iframe src="https://www.youtube.com/embed/OfwbcqTyg3o?autohide=1&amp;border=0&amp;wmode=opaque&amp;enablejsapi=1&amp;mute=1&amp;autoplay=1"
+              allowfullscreen="" frameborder="0">
+            </iframe>
+          </div>
+          <div className={styles.teacerClassesVideos}>
+            <h3>Some examples of my classes</h3>
+            <div className={styles.classVideo}>
+              <h3>Speaking</h3>
+              <iframe src="https://www.youtube.com/embed/JWA5qHp6EGQ?autohide=1&amp;border=0&amp;wmode=opaque&amp;enablejsapi=1&amp;autoplay=1"
+                allowfullscreen="" frameborder="0">
+              </iframe>
+            </div>
+            <div className={styles.classVideo}>
+              <h3>Writing</h3>
+              <iframe src="https://www.youtube.com/embed/QnZ5BUiaZXM?autohide=1&amp;border=0&amp;wmode=opaque&amp;enablejsapi=1&amp;autoplay=1"
+                allowfullscreen="" frameborder="0">
+              </iframe>
+            </div>
+            <div className={styles.classVideo}>
+              <h3>Conversation</h3>
+              <iframe src="https://www.youtube.com/embed/A9-zNVOc47Y?autohide=1&amp;border=0&amp;wmode=opaque&amp;enablejsapi=1&amp;autoplay=1"
+                allowfullscreen="" frameborder="0">
+              </iframe>
+            </div>
+          </div>
+        </div>
+        <div className={styles.students}>
+          <h3> What my students say abuot me</h3>
+          <div className={styles.feedback}>
+            <div className={styles.feedbackPicture}>
+
+            </div>
+            <div className={styles.feedbackText}>
+            <p>Elit laborum labore esse fugiat enim laborum tempor id adipisicing velit nulla eu cupidatat magna. Deserunt ut ad commodo mollit fugiat deserunt elit. Et mollit pariatur mollit consequat et eiusmod non. Exercitation labore incididunt non id cillum voluptate dolor sit sunt consectetur minim exercitation. Exercitation adipisicing adipisicing duis deserunt commodo qui in nostrud. Exercitation nisi mollit elit ut excepteur adipisicing nulla exercitation minim exercitation. Tempor anim eu pariatur esse elit est sit magna pariatur pariatur.</p>
+            </div>
+          </div>
+          <div className={styles.feedback}>
+            <div className={styles.feedbackPicture}>
+
+            </div>
+            <div className={styles.feedbackText}>
+            <p>Elit laborum labore esse fugiat enim laborum tempor id adipisicing velit nulla eu cupidatat magna. Deserunt ut ad commodo mollit fugiat deserunt elit. Et mollit pariatur mollit consequat et eiusmod non. Exercitation labore incididunt non id cillum voluptate dolor sit sunt consectetur minim exercitation. Exercitation adipisicing adipisicing duis deserunt commodo qui in nostrud. Exercitation nisi mollit elit ut excepteur adipisicing nulla exercitation minim exercitation. Tempor anim eu pariatur esse elit est sit magna pariatur pariatur.</p>            
+            </div>
+          </div>
+          <div className={styles.feedback}>
+            <div className={styles.feedbackPicture}>
+
+            </div>
+            <div className={styles.feedbackText}>
+            <p>Elit laborum labore esse fugiat enim laborum tempor id adipisicing velit nulla eu cupidatat magna. Deserunt ut ad commodo mollit fugiat deserunt elit. Et mollit pariatur mollit consequat et eiusmod non. Exercitation labore incididunt non id cillum voluptate dolor sit sunt consectetur minim exercitation. Exercitation adipisicing adipisicing duis deserunt commodo qui in nostrud. Exercitation nisi mollit elit ut excepteur adipisicing nulla exercitation minim exercitation. Tempor anim eu pariatur esse elit est sit magna pariatur pariatur.</p>              
+            </div>
           </div>
         </div>
       </div>
