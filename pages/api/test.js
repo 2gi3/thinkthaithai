bffff23d08c8e515ffef61f15da59c367fdb0750
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-09-03 05:51:01 +0100

    test

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,10 +3,10 @@ import dbConnect from "../../back-end/utilities/dbConnect";
 dbConnect();
 
 const testController = async (req, res) => {
-    res.json({
-        test : 'test',
+    res.status(200).json({
+        test : 'test1',
     });
 }
 
 
-export default testController;
+export default testController; 
