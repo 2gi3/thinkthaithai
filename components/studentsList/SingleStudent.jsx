d2b4a56f833cb026511a946b569addbb8176f604
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-09-19 23:21:31 +0100

    Update and delete student created and tested in the front-end

--- components/studentsList/SingleStudent.jsx
+++ components/studentsList/SingleStudent.jsx
@@ -4,8 +4,74 @@ import styles from './singleStudent.module.scss'
 
 const SingleStudent = () => {
     const [student, setStudent] = useState([])
+    const [Name, setName]= useState()
+    const[Email, setEmail]= useState()
     const router = useRouter()
     let studentId = router.query.id
+
+    const updateStudent = (e, id) => {
+        const JWT = sessionStorage.getItem('token')
+        // const studentId= student.id
+        e.preventDefault()
+
+        const userData = JSON.stringify({
+            name: Name,
+            email : Email,
+            password: student.password
+        })
+        // console.log(userData)
+        fetch(`http://localhost:3000/api/students/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${JWT}`
+            },
+            body: userData,
+            redirect: 'follow'
+        }
+        )
+            .then((res) => {
+                console.log(res)
+                return res.json();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }
+
+    const deleteStudent = (id) => {
+        const JWT = sessionStorage.getItem('token')
+        // const studentId= student.id
+        // e.preventDefault()
+
+        // const userData = JSON.stringify({
+        //     name: Name,
+        //     email : Email,
+        //     password: student.password
+        // })
+        // console.log(userData)
+        fetch(`http://localhost:3000/api/students/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${JWT}`
+            },
+            // body: userData,
+            // redirect: 'follow'
+        }
+        )
+            .then((res) => {
+                console.log(res)
+                router.back()
+                return res.json();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }
+
 
 
     useEffect(
@@ -40,10 +106,41 @@ const SingleStudent = () => {
             <p>{student.email}</p>
             <div className={styles.buttons}>
                 <div>
-                    <button>Update</button>
+                <form
+                onSubmit={e => updateStudent(e, student._id)}
+            >
+                <p>Every input field must be filled</p>
+                <div className={styles.inputBox}>
+                    <label htmlFor="name"> Name</label>
+                    <input type="text" id="name" name="name" defaultValue={student.name}
+                        onChange={event => setName(event.target.value)}
+                    />
+                </div>
+                <div className={styles.inputBox}>
+                    <label htmlFor="name"> Email</label>
+                    <input type="text" id="name" name="name"  defaultValue={student.email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                </div>
+                {/* <div className={styles.inputBox}>
+                    <label htmlFor="name"> Password </label>
+                    <input type="password" name="password" placeholder="Password"
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                </div> */}
+                <button className="primaryButton sauceButton" type="submit" value="submit">
+                    <span>Update</span>
+                </button>
+
+
+            </form>
                 </div>
                 <div>
-                    <button>Delete</button>
+                    <button
+                    onClick={event => deleteStudent(student._id)}
+                    >
+                        Delete
+                    </button>
                 </div>
             </div>
 
--- components/teacher/NewStudentForm.jsx
+++ components/teacher/NewStudentForm.jsx
@@ -3,14 +3,14 @@ import styles from './NewStudentForm.module.scss'
 
 
 
-const NewStudentForm =()=>{
-    
-    
+const NewStudentForm = () => {
+
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const createStudent =(e)=>{
+    const createStudent = (e) => {
         const JWT = sessionStorage.getItem('token')
         e.preventDefault()
 
@@ -21,55 +21,55 @@ const NewStudentForm =()=>{
         })
         // console.log(userData)
         fetch("http://localhost:3000/api/students", {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Authorization": `Bearer ${JWT}`
-                  },
-                body: userData,
-                redirect: 'follow'
-              }
-             )
-            .then((res)=>{
-                console.log(res)                
-                return res.json(); })
-                .catch((err) => {
-                    console.log(err.message);                    
-                })
-    
-
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${JWT}`
+            },
+            body: userData,
+            redirect: 'follow'
+        }
+        )
+            .then((res) => {
+                console.log(res)
+                return res.json();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
     }
-    return(
+
+    return (
         <div className={styles.container}>
-         <form
-                    onSubmit={e => createStudent(e)}
-                    >
-                    <p>Every input field must be filled</p>
-                    <div className={styles.inputBox}>
-                        <label htmlFor="name"> Name</label>
-                        <input type="text" id="name" name="name" //defaultValue={title}
+            <form
+                onSubmit={e => createStudent(e)}
+            >
+                <p>Every input field must be filled</p>
+                <div className={styles.inputBox}>
+                    <label htmlFor="name"> Name</label>
+                    <input type="text" id="name" name="name" //defaultValue={title}
                         onChange={event => setName(event.target.value)}
-                        />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <label htmlFor="name"> Email</label>
-                        <input type="text" id="name" name="name"// defaultValue={title}
+                    />
+                </div>
+                <div className={styles.inputBox}>
+                    <label htmlFor="name"> Email</label>
+                    <input type="text" id="name" name="name"// defaultValue={title}
                         onChange={event => setEmail(event.target.value)}
-                        />
-                    </div>
-<div className={styles.inputBox}>
-                        <label htmlFor="name"> Email</label>
-                        <input type="password" name="password" placeholder="Password"
-                            onChange={event => setPassword(event.target.value)}
-                        />
-                    </div>
-                    <button className="primaryButton sauceButton" type="submit" value="submit">
-                            <span>Create </span>
-                        </button>
+                    />
+                </div>
+                <div className={styles.inputBox}>
+                    <label htmlFor="name"> Password </label>
+                    <input type="password" name="password" placeholder="Password"
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                </div>
+                <button className="primaryButton sauceButton" type="submit" value="submit">
+                    <span>Create </span>
+                </button>
 
 
-                    </form>
+            </form>
         </div>
     )
 }
