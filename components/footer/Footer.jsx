67d626d4a80208ef3515aa2dcdbfe1148b742adb
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-09-28 23:06:55 +0100

    cloudinary branch merged

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <Contacts />
             <div className={styles.copyRights}>
                 <p className={styles.copyRightsBy}>Website by:<a> Giuseppe Ippolito.</a></p>
-                <p className={styles.copyRightsYear}>&copy; 2022 All right reserved</p>
+                <p className={styles.copyRightsYear}>&copy; 2022 All rights reserved</p>
             </div>
         </footer>
 
