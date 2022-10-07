4bb16a8d03d76156d3d2eb8c327f4b9e05d040fd
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-10-08 00:53:19 +0100

    API test

--- pages/api/feedbacks/index.js
+++ pages/api/feedbacks/index.js
@@ -25,7 +25,7 @@ export default async (req, res) => {
             try {
                 const feedback = await Feedback.find({});
                 res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+                // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
                 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
                 res.status(200).json({ success: true, data: feedback })
@@ -71,7 +71,7 @@ export default async (req, res) => {
                     );
                     const student = await Feedback.create(body);
                     res.setHeader('Access-Control-Allow-Origin', '*');
-                    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+                    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
                     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
                     res.status(201).json({ success: true, data: student });
--- pages/api/students/[id].js
+++ pages/api/students/[id].js
@@ -8,7 +8,7 @@ dbConnect();
 
 export default async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
     const {
