68334ff72acf87962c2aff5a4f142b9817461733
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-10-08 03:37:27 +0100

    test

--- pages/api/students/login.js
+++ pages/api/students/login.js
@@ -8,9 +8,10 @@ const JWTSecret = process.env.JWT_SECRET;
 dbConnect();
 
 export default (req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next()
     Student.findOne({ email: req.body.email }).then(
       (student) => {
         if (!student) {
