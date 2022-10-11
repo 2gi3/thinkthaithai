3c849293c2b5f3ad627ef1663f3e67030c053a60
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2022-10-11 23:39:31 +0100

    changes to next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,17 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-module.exports = {
-  images: {
-    domains: ['www.paypalobjects.com', "res.cloudinary.com"],
-  },
-}
+// module.exports = {
+//   images: {
+//     domains: ['www.paypalobjects.com', "res.cloudinary.com"],
+//   },
+// }
 
 module.exports ={
   
+  images: {
+    domains: ['www.paypalobjects.com', "res.cloudinary.com"],
+  },
   // res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
